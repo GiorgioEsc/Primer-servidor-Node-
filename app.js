ea531462924cfba241e7cfeae663af4575df0d5f
Antonio Esc <jeantonio.666@gmail.com>
2022-07-17 19:16:16 -0500

    listos para producción

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express')
 
 const app = express()
+const multer  = require('multer')
+const sharp = require('sharp')
+const fs = require('fs')
+
+const storageStrategy = multer.memoryStorage()
+const upload = multer({storage: storageStrategy })
+
+app.use(express.json())
+
 
 app.get('/', function(req, res){
-
     res.send('Hola Mundo!')
 })
 
-app.listen(3000)
+app.post('/imagen',upload.single('imagen'), async function (req, res){
+    const body= req.body
+    const imagen= req.file
+
+    const processedImage = sharp(imagen.buffer).resize(200, 10,{
+        fit: "contain",
+        background: "#fff"
+    })
+    const resizedImageBuffer = await processedImage.toBuffer()
+    
+    fs.writeFileSync('uploads/prueba.png',resizedImageBuffer)
+
+    console.log(resizedImageBuffer)
+    res.send({resizedImageBuffer})
+})
+
+const port = process.env.port || 3000
+app.listen(port, function(){
+    console.log("Servidor escuchando en el puerto", port)
+})
 
