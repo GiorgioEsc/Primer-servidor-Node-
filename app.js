fec7965172d39ffa38b7f66028f042f702fa398d
Antonio Esc <jeantonio.666@gmail.com>
2022-07-17 19:23:24 -0500

    listos para producción 2.0

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/imagen',upload.single('imagen'), async function (req, res){
     res.send({resizedImageBuffer})
 })
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, function(){
     console.log("Servidor escuchando en el puerto", PORT)
 })
